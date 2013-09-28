820f8412317586c7b416e812a2e75ca87fb17226
Joe Burgess <joe@flatironschool.com>
2013-09-28 14:38:44 -0400

    Modified json_generator to use more lifelike data

--- json_generator.rb
+++ json_generator.rb
@@ -2,36 +2,22 @@ require 'json'
 
 MOODS = %w[Hungover Awesome Tired Dragging Meh Exhausted Energized]
 
+checkins = JSON.parse(IO.read("checkins.json"))
 
+json_out = {}
 
-def process_items items
-  json_out = {}
-
-  items.each do |item|
-    json_out[item]={
+checkins["response"]["checkins"]["items"].each do |checkin|
+  item = {
       "sleep" =>Random.new.rand(7200..36000),
       "quality" => Random.new.rand(10..100),
       "mood" => MOODS.sample
-    }
-  end
+  }
+  item["venue"]=checkin["venue"]
 
-  json_out
-end
-
-
-File.open("friends.json","w") do |f|
-  json_out = process_items %w[Avi Adam Jeff Rebekah Blake zZak Joe Ashley Scott Spencer Bob]
-  f.write(json_out.to_json)
+  json_out[checkin["venue"]["id"]] = item
 end
 
 File.open("locations.json","w") do |f|
-  locations = []
-  locations << "The Grey Bar"
-  locations << "Hogpit"
-  locations << "Bushwick Country Club"
-  locations << "The Flatiron School"
-  locations << "Stumptown Coffee Roasters"
-  locations << "Ace Hotel"
-  json_out = process_items locations
   f.write(json_out.to_json)
 end
+
