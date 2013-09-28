d1617ca3bfc8754d070d2a021ba7af71e5918d91
Joe Burgess <joe@flatironschool.com>
2013-09-28 11:23:54 -0400

    Finished json_generator to deal with IV v DV
      - Generic process_items method now

--- json_generator.rb
+++ json_generator.rb
@@ -1,34 +1,37 @@
 require 'json'
 
-friends = %w[Avi Adam Jeff Rebekah Blake zZak Joe Ashley Scott Spencer Bob]
-moods = %w[Hungover Awesome Tired Dragging Meh Exhausted Energized]
+MOODS = %w[Hungover Awesome Tired Dragging Meh Exhausted Energized]
 
-locations = []
-locations << "The Grey Bar"
-locations << "Hogpit"
-locations << "Bushwick Country Club"
-locations << "The Flatiron School"
-locations << "Stumptown Coffee Roasters"
-locations << "Ace Hotel"
 
 
 def process_items items
   json_out = {}
 
-  item.each do |item|
-    json_out[friend]={
+  items.each do |item|
+    json_out[item]={
       "sleep" =>Random.new.rand(7200..36000),
       "quality" => Random.new.rand(10..100),
-      "mood" => moods.sample
+      "mood" => MOODS.sample
     }
   end
 
-  File.open("items.json","w") do |f|
-    f.write(json_out.to_json)
-  end
+  json_out
 end
 
 
-  File.open("friends.json","w") do |f|
-    f.write(json_out.to_json)
-  end
+File.open("friends.json","w") do |f|
+  json_out = process_items %w[Avi Adam Jeff Rebekah Blake zZak Joe Ashley Scott Spencer Bob]
+  f.write(json_out.to_json)
+end
+
+File.open("locations.json","w") do |f|
+  locations = []
+  locations << "The Grey Bar"
+  locations << "Hogpit"
+  locations << "Bushwick Country Club"
+  locations << "The Flatiron School"
+  locations << "Stumptown Coffee Roasters"
+  locations << "Ace Hotel"
+  json_out = process_items locations
+  f.write(json_out.to_json)
+end
