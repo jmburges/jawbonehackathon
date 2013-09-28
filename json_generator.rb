924f94c34f2d332a6a0630f435dbd5622e5764e2
Joe Burgess <joe@flatironschool.com>
2013-09-28 11:19:13 -0400

    Refactoring to separate generic methods

--- json_generator.rb
+++ json_generator.rb
@@ -11,20 +11,24 @@ locations << "The Flatiron School"
 locations << "Stumptown Coffee Roasters"
 locations << "Ace Hotel"
 
-json_out = {}
-300.times do |i|
-  date = Time.new() - (i*86400)
-  sleep_today = Random.new.rand(7200..36000)
-  quality = Random.new.rand(10..100)
-  json_out[date]= {
-    "sleep" => sleep_today,
-    "sleep_quality" => quality,
-    "friends" => friends.sample(Random.new.rand(0..11)),
-    "moods" => moods.sample(Random.new.rand(0..7))
-  }
+
+def process_items items
+  json_out = {}
+
+  item.each do |item|
+    json_out[friend]={
+      "sleep" =>Random.new.rand(7200..36000),
+      "quality" => Random.new.rand(10..100),
+      "mood" => moods.sample
+    }
+  end
+
+  File.open("items.json","w") do |f|
+    f.write(json_out.to_json)
+  end
 end
 
-File.open("temp.json","w") do |f|
-  f.write(json_out.to_json)
-end
 
+  File.open("friends.json","w") do |f|
+    f.write(json_out.to_json)
+  end
