fee95777118686a19991b5530fd6e4a803caba34
Joe Burgess <joe@flatironschool.com>
2013-09-29 08:26:06 -0400

    Implemented locations algorithm horribly

--- app.rb
+++ app.rb
@@ -12,6 +12,9 @@ class JawboneApp < Sinatra::Base
       ENV['JAWBONE_CLIENT_ID'], 
       ENV['JAWBONE_CLIENT_SECRET'], 
       :scope => "basic_read mood_read sleep_read"
+    provider :foursquare,
+      ENV['FOURSQUARE_CONSUMER_KEY'],
+      ENV['FOURSQUARE_CONSUMER_SECRET']
   end
   
   configure do
@@ -25,19 +28,126 @@ class JawboneApp < Sinatra::Base
   end
 
   get '/' do
-    client = Jawbone::Client.new session[:jawbone_token]
-    puts "#"*15
-    puts client.sleeps
-    puts "#"*15
+
     haml :index
   end
 
+  get '/checkins' do
+
+  end
+
   get '/locations' do
-    File.read("locations.json")
+    # File.read("locations.json")
+    client = Jawbone::Client.new session[:jawbone_token]
+    foursquare_client = Foursquare2::Client.new(:oauth_token => session[:foursquare_token])
+    start_date = DateTime.now - 300
+    checkins=[]
+    foursquare_thread = Thread.new{
+      checkins = foursquare_client.user_checkins({
+      :afterTimestamp => start_date.strftime("%s"),
+      :limit => 250
+    })["items"]
+    items = checkins.size
+
+    while items == 250
+      result=foursquare_client.user_checkins({
+        :afterTimestamp => start_date.strftime("%s"),
+        :limit => 250,
+        :offset => checkins.size
+      })["items"]
+      items = result.size
+      checkins+=result
+    end
+    }
+    sleeps=[]
+    jawbone_thread = Thread.new{
+      temp =client.sleeps({
+      :start_time => start_date.strftime("%s"),
+      :end_time => DateTime.now.strftime("%s")
+    })
+    sleeps = temp["data"]["items"]
+    while !temp["data"]["links"].nil? and !temp["data"]["links"]["next"].nil?
+      temp= client.sleeps_next(temp["data"]["links"]["next"])
+      sleeps += temp["data"]["items"]
+    end
+    }
+    jawbone_thread.join
+    foursquare_thread.join
+    location_sleeps ={}
+    unique_locations = {}
+    location_counter = {}
+    current_date = DateTime.strptime(checkins[0]["createdAt"].to_s,"%s")
+    locations = []
+    checkins.each do |checkin|
+      if checkin["venue"].nil?
+        break;
+      end
+      if location_counter[checkin["venue"]["id"]].nil?
+        location_counter[checkin["venue"]["id"]]=1
+        unique_locations[checkin["venue"]["id"]]=checkin["venue"]
+      else
+        location_counter[checkin["venue"]["id"]]+=1
+      end
+      createdat = DateTime.strptime(checkin["createdAt"].to_s,"%s")
+      if current_date.day != createdat.day 
+        current_date = createdat
+        locations.clear
+      end
+      sleep_index=0
+      sleeps[sleep_index..-1].each_index do |i|
+        sleep_start = DateTime.strptime(sleeps[i]["time_created"].to_s, "%s")
+        sleep_end = DateTime.strptime(sleeps[i]["time_completed"].to_s, "%s")
+        if (sleep_start-createdat<1 and sleep_start-createdat>0)
+
+          if sleep_start.day == createdat.day
+
+            if !locations.include? checkin["venue"]["id"]
+
+              if location_sleeps[checkin["venue"]["id"]].nil?
+                location_sleeps[checkin["venue"]["id"]] = [sleeps[i]["details"]["duration"]]
+              else
+                location_sleeps[checkin["venue"]["id"]] << sleeps[i]["details"]["duration"]
+              end
+                locations << checkin["venue"]["id"]
+            end
+          else
+            if sleep_end.hour <=12 or sleeps[i]["details"]["duration"]>=3600
+
+              if !locations.include? checkin["venue"]["id"]
+
+              if location_sleeps[checkin["venue"]["id"]].nil?
+                location_sleeps[checkin["venue"]["id"]] = [sleeps[i]["details"]["duration"]]
+              else
+                location_sleeps[checkin["venue"]["id"]] << sleeps[i]["details"]["duration"]
+              end
+                locations << checkin["venue"]["id"]
+              end
+            end
+          end
+          sleep_index = i;
+        end
+      end
+    end
+
+    json_out={}
+    location_sleeps.each_pair do |loc_id, loc_sleeps|
+      item = {
+        "sleep" =>loc_sleeps.inject(:+).to_f/loc_sleeps.length,
+        "quality" => Random.new.rand(10..100),
+        "mood" => "Awesome",
+        "checkins" => location_counter[loc_id],
+        "venue"=> unique_locations[loc_id]
+      }
+      json_out[loc_id]=item
+    end
+
+
+    content_type :json
+    json_out.to_json
   end
 
   get '/friends' do
-    File.read("friends.json")
+
   end
 
   get '/custom.css' do
@@ -51,6 +161,12 @@ class JawboneApp < Sinatra::Base
     session[:jawbone_id]=auth["uid"]
     session[:first_name] = auth["info"]["first_name"]
     session[:last_name] = auth["info"]["last_name"]
+  end
+
+  get '/auth/foursquare/callback' do
+    auth = request.env['omniauth.auth']
+    session[:foursquare_token] = auth["credentials"]["token"]
+    session[:foursquare_id]=auth["uid"]
   end
 
   helpers do
