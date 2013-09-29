0fc161302fdc9b51ee64b6d2813aa70e8c89ba12
Joe Burgess <joe@flatironschool.com>
2013-09-28 20:55:35 -0400

    Adding in checkin count

--- json_generator.rb
+++ json_generator.rb
@@ -12,7 +12,13 @@ checkins["response"]["checkins"]["items"].each do |checkin|
       "quality" => Random.new.rand(10..100),
       "mood" => MOODS.sample
   }
+  if json_out[checkin["venue"]["id"]].nil?
+    item["checkins"]=0
+  else
+    item["checkins"]=json_out[checkin["venue"]["id"]]["checkins"]+1
+  end
   item["venue"]=checkin["venue"]
+
 
   json_out[checkin["venue"]["id"]] = item
 end
